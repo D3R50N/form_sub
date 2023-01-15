d93f2d7f8cb35b1b9feb67e1aa2af96f22e8d12c
Max Anderson MOUMOUNI <happydev.229@gmail.com>
2023-01-15 02:19:24 +0100

    Some updates

--- config/index.js
+++ config/index.js
@@ -7,6 +7,7 @@ const config = {
         user : process.env.DB_USER || "root",
         password: process.env.DB_PASSWORD || "",
         database: process.env.DB_NAME || "form_submission",
+        port : process.env.DB_PORT || 3306
     },
 }
 
